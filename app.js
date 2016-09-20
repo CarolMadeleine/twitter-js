83e9a0028c0629c67adfc09f5460ea62e6d8b1c0
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-20 11:03:05 -0500

    subiendo preventDeafault

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 window.addEventListener("load", function(){
-		var boton = document.getElementById("enviar");
-		boton.addEventListener("click", function(e){
-				e.preventDefault();//para que no aparezca en la url a pesar que es tipo submit
-				var textArea = document.getElementById("mensaje")
-				var texto = textArea.value;
-				agregarMensaje(textArea.value);
-				textArea.value = "";
-				
-		});
-
-		function agregarMensaje (texto){
-			var div = document.createElement("div");
-			div.innerText = texto;
-			var mensajes =  document.getElementById("mensajes");
-			mensajes.insertBefore(div, mensajes.childNodes[0]);
-		}
+    var boton = document.getElementById("enviar");
+    boton.addEventListener("click", function(e){
+    	e.preventDefault();
+        var texto = document.getElementById("mensaje").value;
+        var box = document.getElementById("resultado");
+    });
 });
+
