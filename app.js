09435908b5381b409e6ce170b4eed731d9201c24
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-20 12:42:29 -0500

    añadiendo contador

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 window.addEventListener("load", function(){
     var boton = document.getElementById("enviar");
+    var mensaje = document.getElementById("mensaje");
     boton.addEventListener("click", function(e){
     	e.preventDefault();
-        var texto = document.getElementById("mensaje").value;
-        var box = document.getElementById("resultado");
+ 		var contenedor = mensaje.value;
+        box(contenedor);
+        mensaje.value="";
+        boton.disabled ="true";
+    });
+    function box(contenedor){
+    	var div = document.createElement("div");
+    	div.innerText = contenedor;
+    	var resultado = document.getElementById("resultado");
+    	resultado.insertBefore(div, resultado.childNodes[0]);
+    }
+    mensaje.addEventListener("keydown", function(){
+    	boton.disabled = false;
+    	var limite = 140;
+    	var longitud = mensaje.value.length;
+    	var contador = document.getElementById("contador");//140
+
+    	if (longitud<=limite) {
+    		contador.innerText = limite - longitud;
+    	}
     });
 });
 
+/*var hermano = document.createElement("div");
+        hermano.innerText = texto;
+
+        if(!box.childNodes[0]){
+            box.appendChild(hermano);
+        }
+        else{
+            box.insertBefore(hermano, box.childNodes[0]);
+        }
+
+        hermano.classList.add("tweets");*/
