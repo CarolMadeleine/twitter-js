2f7200d4d8803959917363bc8b9b79aeb01ad86b
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-21 19:37:00 -0500

    subiendo cambios finales de twwiter

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 window.addEventListener("load", function(){
     var boton = document.getElementById("enviar");
     var mensaje = document.getElementById("mensaje");
+    var resultado = document.getElementById("caja");
+    var contadorClick = 1;
     boton.addEventListener("click", function(e){
+
+        var contadorTweet = document.getElementById("numeroContador");
+        contadorTweet.innerHTML= contadorClick;
+        contadorClick++;
+            
     	e.preventDefault();
  		var contenedor = mensaje.value;
         box(contenedor);
@@ -10,12 +17,51 @@ window.addEventListener("load", function(){
         textoNormal();
     });
     function box(contenedor){
-    	var div = document.createElement("div");
-    	div.innerText = contenedor;
-    	var resultado = document.getElementById("resultado");
-    	var n = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
-        div.innerText = contenedor + "  " + n;
-    	resultado.insertBefore(div, resultado.childNodes[0]);
+    	var newTweet = document.createElement("div");
+    	newTweet.classList.add("caja");
+    	resultado.insertBefore(newTweet, resultado.childNodes[0]);
+        
+    	var imagenAbajo= document.createElement("img");
+        imagenAbajo.classList.add("imagent2")
+        imagenAbajo.setAttribute("src", "image/t2.png")
+        newTweet.insertBefore(imagenAbajo, newTweet.childNodes[0]);
+
+        var entrada = document.createElement("div");
+        entrada.classList.add("entrada")
+        entrada.innerHTML= contenedor;
+        newTweet.insertBefore(entrada, newTweet.childNodes[0]);
+
+    	var horaActual = new Date ();
+        var hora = horaActual.getHours();
+        var minuto = horaActual.getMinutes();
+        var horaContenedor = document.createElement("span");
+        horaContenedor.classList.add("hora")
+        horaContenedor.innerHTML= hora + ":"+ minuto;
+        newTweet.insertBefore(horaContenedor, newTweet.childNodes[0]);
+
+        var arroba = document.createElement("div");
+        arroba.classList.add("arroba")
+        arroba.innerHTML= "@Carol Madeleine •" + "&nbsp";
+        newTweet.insertBefore(arroba, newTweet.childNodes[0]);
+
+        var nombre = document.createElement("div");
+        nombre.classList.add("nombre")
+        nombre.innerHTML= " Carol Madeleine" + "&nbsp";
+        newTweet.insertBefore(nombre, newTweet.childNodes[0]);
+
+        var imagen= document.createElement("img");
+        imagen.classList.add("imagenPerfil")
+        imagen.setAttribute("src", "image/perfil.png")
+        newTweet.insertBefore(imagen, newTweet.childNodes[0]);
+
+        /*var imagen= document.createElement("img");
+        imagen.setAttribute("src", "perfil.png");
+        var bloq=document.getElementById("bloque");
+        bloq.appendChild(imagen, newTweet.childNodes[0]); con html*/
+
+
+    	// var n = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
+        // div.innerText = contenedor + "  " + n;  // para la hora
 
     }
     mensaje.addEventListener("keyup", function(){
@@ -41,9 +87,7 @@ window.addEventListener("load", function(){
     		contador.style.color = "green";
     	}
     });
-
     mensaje.addEventListener("keydown", textoGrande);
-
  	function textoGrande(){
  		mensaje.style.cssText = "height:auto";
      	mensaje.style.cssText = "height: " + mensaje.scrollHeight + "px";
@@ -51,5 +95,5 @@ window.addEventListener("load", function(){
 	function textoNormal(){
  		mensaje.style.cssText = "height:auto";
 	}
-});
 
+});
